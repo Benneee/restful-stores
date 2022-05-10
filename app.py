5d3534c4c5ba9e00883b7e2a4c16b9a14dd34ad2
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2022-05-10 23:59:52 +0100

    Feat: added endpoints for creating and getting an item

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+items = []
 
 # class Student(Resource):
 #     def get(self, name):
@@ -11,6 +12,29 @@ api = Api(app)
 
 
 # api.add_resource(Student, '/student/<string:name>') # example URL: http://localhost:5000/student/benneee
+
+
+class Item(Resource):
+    def get(self, name):
+        for item in items:
+            if item['name'] == name:
+                return {"item": item}
+
+        return {
+            "message": 'Item not found!',
+            "item": None
+        }, 404
+
+
+    def post(self, name):
+        item = { "name": name, "price": 12.00 }
+        items.append(item)
+        return {
+            "message": "Item added",
+            "item": item
+        }, 201
+
+api.add_resource(Item, '/item/<string:name>') # example URL: http://localhost:5000/item/benneee
 
 if __name__ == '__main__':
     app.run(debug=True)
