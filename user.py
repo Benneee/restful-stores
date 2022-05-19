94462ec34725aef67451c3332b926f4d6d00b516
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2022-05-19 08:59:36 +0100

    Feat: created class methods to find users by id and username from the DB

--- user.py
+++ user.py
@@ -1,5 +1,42 @@
+from multiprocessing import connection
+import sqlite3
+
 class User:
     def __init__(self, _id, username, password):
         self.id = _id
         self.username = username
         self.password = password
+
+    # We need methods that will interact with the database to find users
+    @classmethod
+    def find_by_username(cls, username):
+        connection = sqlite3.connect('app.db')
+        cursor = connection.cursor()
+
+        select_query = "SELECT * FROM users WHERE username=?"
+        result = cursor.execute(select_query, (username,))  # The parameters always have to be in a tuple
+        row = result.fetchone()
+        if row:
+            # user = cls(row[0], row[1], row[2])
+            user = cls(*row)
+        else:
+            user = None
+
+        connection.close()
+        return user
+
+    @classmethod
+    def find_by_id(cls, _id):
+        connection = sqlite3.connect('app.db')
+        cursor = connection.cursor()
+
+        select_query = "SELECT * FROM users WHERE id=?"
+        result = cursor.execute(select_query, (_id,))
+        row = result.fetchone()
+        if row:
+            user = cls(*row)
+        else:
+            user = None
+
+        connection.close()
+        return user
